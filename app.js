d4a8251d5b3f16005bd8ccf98a6019ce15f189c5
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2018-05-24 23:59:15 -0700

    used async fetch/wait to add detailed route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express')
 let app = express()
-let router = express.Router()
+
 let playerService = require('./services/playerService.js')
 // set up our routes
 app.get('/', function (req, res) {
@@ -11,15 +11,12 @@ app.get('/allPlayers', function (req, res) {
   res.send(playerService.getPlayers())
 })
 
-app.get('/createPlayer/:playerId', function (req, res) {
-  playerService.populatePlayer(req.params.playerId)
-  res.send('successfull creation!')
-})
-
 app.get('/getPlayer/:playerId', function (req, res) {
-  res.send(playerService.getPlayer(req.params.playerId))
+  playerService.getPlayer(req.params.playerId).then(function (player) {
+    res.send(player)
+  })
 })
 
-app.listen(8080, function() {
+app.listen(8080, function () {
   console.log('Listening on port 8080!')
 })
--- services/playerService.js
+++ services/playerService.js
@@ -1,109 +1,75 @@
 let config = require('../config/config.js')
-var rp = require('request-promise')
+let fetch = require('node-fetch')
 let fs = require('fs')
 let teamStore = require('json-fs-store')('./Resources/Teams')
 let playerStore = require('json-fs-store')('./Resources/Players')
 
-
-exports.populatePlayers = function() {
+exports.populatePlayers = function () {
   createPlayers()
 }
 
-exports.getPlayers = function() {
+exports.getPlayers = function () {
   var obj = JSON.parse(fs.readFileSync('Resources/Players/players.json', 'utf8'))
   return obj.players
 }
 
-exports.populatePlayer = function(playerId) {
-  console.log('the call to populate player has been made... about to trigger the internal createPlayer()....')
-   createPlayer(playerId)
+exports.getPlayer = async function (playerId) {
+  let url = config.baseUrl + 'player/' + playerId + '?Authorization=' + config.apiKey
+  let response = await fetch(url)
+  let data = await response.json()
+  return data
 }
 
-exports.getPlayer = function(playerId) {
-  console.log('... I am inside playerService.getPlayer(), about to trigger the internal getPlayerDetails()...')
-  return getPlayerDetails(playerId)
-}
-
-function getPlayerDetails(playerId) {
-  console.log('About to try read the written JSON object')
-  let objPath = 'Resources/Players/' + playerId + '.json'
-  var obj = JSON.parse(fs.readFileSync(objPath, 'utf8'))
-  return obj
-}
-
-function createPlayer(playerId) {
-  console.log('I am inside the createPlayer internally... about to make the url req...')
-  let url = config.baseUrl + 'player/' + playerId + "?Authorization=" + config.apiKey
-  rp({
-    "method": "GET",
-    "uri": url,
-    "json": true
-  }).then(function (response) {
-    console.log('about to try write the response to the call for the detailed player...')
-    let obj = {
-      id: playerId,
-      player: response
-    }
-    playerStore.add(obj, function(err) {
-      if (err) throw err
-      console.log('the player file has been written')
-    })
-  })
-
-}
-
-
-function createPlayers() {
+function createPlayers () {
   logBeginning()
 
-let playerArr = []
-teamStore.list(function(err, objects) {
-  if (err) throw err
-  for (var i = 0; i < objects.length; i++) {
+  let playerArr = []
+  teamStore.list(function (err, objects) {
+    if (err) throw err
+    for (var i = 0; i < objects.length; i++) {
     // we extract info that we want accesible to our player from the teams
-    let squad = objects[i].squad
-    let teamName = objects[i].id
-    let team_id = objects[i].team_id
-    let league = objects[i].league
-    for (var x = 0; x < squad.length; x++) {
-      let player = squad[x]
+      let squad = objects[i].squad
+      let teamName = objects[i].id
+      let team_id = objects[i].team_id
+      let league = objects[i].league
+      for (var x = 0; x < squad.length; x++) {
+        let player = squad[x]
 
-      // edit some player info before we add it to our array
-      adjustPosition(player)
-      player.team = teamName
-      player.team_id = team_id
-      player.league = league
-      playerArr.push(player)
+        // edit some player info before we add it to our array
+        adjustPosition(player)
+        player.team = teamName
+        player.team_id = team_id
+        player.league = league
+        playerArr.push(player)
+      }
     }
-  }
-  // send the player array to be written in JSON in the proper Resource folder
-  writePlayers(playerArr)
-  console.log('number of players is... ' + playerArr.length)
-})
+    // send the player array to be written in JSON in the proper Resource folder
+    writePlayers(playerArr)
+    console.log('number of players is... ' + playerArr.length)
+  })
 }
 
-
-function adjustPosition(player) {
+function adjustPosition (player) {
   switch (player.position) {
-    case "G":
-      player.position = "Goalkeeper"
+    case 'G':
+      player.position = 'Goalkeeper'
       break
 
-    case "D":
-      player.position = "Defender"
+    case 'D':
+      player.position = 'Defender'
       break
 
-    case "M":
-      player.position = "Midfielder"
+    case 'M':
+      player.position = 'Midfielder'
       break
 
-    case "A":
-      player.position = "Attacker"
+    case 'A':
+      player.position = 'Attacker'
       break
   }
 }
 
-function writePlayers(playerArr) {
+function writePlayers (playerArr) {
   let obj = {
     id: 'players',
     players: playerArr
@@ -111,7 +77,7 @@ function writePlayers(playerArr) {
   playerStore.add(obj, function (err) { if (err) throw err })
 }
 
-function logBeginning() {
+function logBeginning () {
   console.log()
   console.log()
   console.log('attemptin to recover the files from the Teams directory in resources')
