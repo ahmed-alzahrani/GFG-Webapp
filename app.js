b93a4011850366c32f033c0adb4e023390f51288
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2018-05-24 23:19:44 -0400

    testing detailedPlayer routing, with 1 call to create and 1 to return

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 let express = require('express')
 let app = express()
+let router = express.Router()
 let playerService = require('./services/playerService.js')
 // set up our routes
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.get('/team/:teamId', function (req, res) {
-  let message = 'you queried for team with the id ' + req.params.teamId
-  res.send(message)
-})
-
 app.get('/allPlayers', function (req, res) {
   res.send(playerService.getPlayers())
+})
+
+app.get('/createPlayer/:playerId', function (req, res) {
+  playerService.populatePlayer(req.params.playerId)
+  res.send('successfull creation!')
+})
+
+app.get('/getPlayer/:playerId', function (req, res) {
+  res.send(playerService.getPlayer(req.params.playerId))
 })
 
 app.listen(8080, function() {
--- services/playerService.js
+++ services/playerService.js
@@ -1,4 +1,5 @@
 let config = require('../config/config.js')
+var rp = require('request-promise')
 let fs = require('fs')
 let teamStore = require('json-fs-store')('./Resources/Teams')
 let playerStore = require('json-fs-store')('./Resources/Players')
@@ -12,6 +13,45 @@ exports.getPlayers = function() {
   var obj = JSON.parse(fs.readFileSync('Resources/Players/players.json', 'utf8'))
   return obj.players
 }
+
+exports.populatePlayer = function(playerId) {
+  console.log('the call to populate player has been made... about to trigger the internal createPlayer()....')
+   createPlayer(playerId)
+}
+
+exports.getPlayer = function(playerId) {
+  console.log('... I am inside playerService.getPlayer(), about to trigger the internal getPlayerDetails()...')
+  return getPlayerDetails(playerId)
+}
+
+function getPlayerDetails(playerId) {
+  console.log('About to try read the written JSON object')
+  let objPath = 'Resources/Players/' + playerId + '.json'
+  var obj = JSON.parse(fs.readFileSync(objPath, 'utf8'))
+  return obj
+}
+
+function createPlayer(playerId) {
+  console.log('I am inside the createPlayer internally... about to make the url req...')
+  let url = config.baseUrl + 'player/' + playerId + "?Authorization=" + config.apiKey
+  rp({
+    "method": "GET",
+    "uri": url,
+    "json": true
+  }).then(function (response) {
+    console.log('about to try write the response to the call for the detailed player...')
+    let obj = {
+      id: playerId,
+      player: response
+    }
+    playerStore.add(obj, function(err) {
+      if (err) throw err
+      console.log('the player file has been written')
+    })
+  })
+
+}
+
 
 function createPlayers() {
   logBeginning()
@@ -63,9 +103,6 @@ function adjustPosition(player) {
   }
 }
 
-function getPlayers() {
-
-}
 function writePlayers(playerArr) {
   let obj = {
     id: 'players',
